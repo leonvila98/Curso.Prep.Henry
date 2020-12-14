b2dc39826ac9e49dc0c44ebdf5197f015e9b29f5
leon <leon.18@live.com.ar>
2020-12-14 15:29:14 -0300

    CP commiteado por leonvila98 a las 1607970554570

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -27,7 +27,7 @@ function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
-  var nuevoArray = array.map(function(elemento) {
+  var nuevoArray = array.map(function(elemento){
     return cb(elemento);
   });
   return nuevoArray;
